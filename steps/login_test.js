9c2be2324be957b67f9e309e302b81258071b9f2
mariafigueiredoflexmobile <mariafigueiredoflexmobile@gmail.com>
2025-03-25 14:59:55 -0300

    Updates

--- steps/login_test.js
+++ steps/login_test.js
@@ -1,53 +1,53 @@
-Feature('login')//.tag('@login');
-const {I, login_page, home_page} = inject() //importação de módulos
+// Feature('login')//.tag('@login');
+// const {I, login_page, home_page} = inject() //importação de módulos
 
 
-BeforeSuite(() => { //execução antes dos testes
-   console.log('Before Suite')
-});
+// BeforeSuite(() => { //execução antes dos testes
+//    console.log('Before Suite')
+// });
 
-Before(() => {
-   console.log('Before')
-});
+// Before(() => {
+//    console.log('Before')
+// });
 
-AfterSuite(() => {  //execução depois dos testes
-   console.log('After Suite')
-});
+// AfterSuite(() => {  //execução depois dos testes
+//    console.log('After Suite')
+// });
 
-After(() => {
-   console.log('After')
-});
+// After(() => {
+//    console.log('After')
+// });
 
-Scenario('Login with sucess',  (home_page) => { //login com sucesso
+// Scenario('Login with sucess',  (home_page) => { //login com sucesso
 
-   I.runOnAndroid(() => {
-      console.log('Estou no Android')
-}),
+//    I.runOnAndroid(() => {
+//       console.log('Estou no Android')
+// }),
 
-   login_page.doLogin('teste@teste.com', '123456')
-   I.tap('~entrar')
-   home_page.checkLoginSucess()
+//    login_page.doLogin('teste@teste.com', '123456')
+//    I.tap('~entrar')
+//    home_page.checkLoginSucess()
 
-   // pause()
-   // I.touchPerform([
-   //    {
-   //    action: 'longPress',
-   //    options: {x:300, y: 1100}
-   //    },
-   //    {
-   //       action: 'MoveTo',
-   //       options: {x:300, y: 250}
-   //       },
-   //       {action: 'realease'}
-   // ])
-   //I.wait(5)
+//    // pause()
+//    // I.touchPerform([
+//    //    {
+//    //    action: 'longPress',
+//    //    options: {x:300, y: 1100}
+//    //    },
+//    //    {
+//    //       action: 'MoveTo',
+//    //       options: {x:300, y: 250}
+//    //       },
+//    //       {action: 'realease'}
+//    // ])
+//    //I.wait(5)
   
-});//.tag('@login_sucesso');
+// });//.tag('@login_sucesso');
 
-Scenario('Login with error',  () => { //login com erro
-   login_page.doLogin('xteste@teste.com', '123456')
+// Scenario('Login with error',  () => { //login com erro
+//    login_page.doLogin('xteste@teste.com', '123456')
 
-   I.tap('~entrar') //botão entrar
-   login_page.ckeckLoginError()
+//    I.tap('~entrar') //botão entrar
+//    login_page.ckeckLoginError()
    
-})//.tag('@login_erro');
+// })//.tag('@login_erro');
--- steps/students_test.js
+++ steps/students_test.js
@@ -1,17 +1,17 @@
-Feature('Students');
+// Feature('Students');
 
-const{I, login_page, home_page} =inject()
-const code = require('../Utils/code')
-const name = require('../Utils/name')
+// const{I, login_page, home_page} =inject()
+// const code = require('../Utils/code')
+// const name = require('../Utils/name')
 
 
-Scenario('Add student with success', () => {
+// Scenario('Add student with success', () => {
 
-    const mycode = code.getCode()
-    const myname = name.getName() 
+//     const mycode = code.getCode()
+//     const myname = name.getName() 
 
-    login_page.doLogin('teste@teste.com', '123456')
-    home_page.registerStudent(mycode, myname)
-    home_page.searchStudent(myname, mycode)
+//     login_page.doLogin('teste@teste.com', '123456')
+//     home_page.registerStudent(mycode, myname)
+//     home_page.searchStudent(myname, mycode)
 
-});
+// });
